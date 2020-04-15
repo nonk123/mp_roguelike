602aac6099b61e2a71cf3ee7b4082c0804850489
nonk123 <Sergeyii321@gmail.com>
2020-04-15 07:21:10 +0300

    Add player entities

--- mp_roguelike/consumers.py
+++ mp_roguelike/consumers.py
@@ -2,23 +2,39 @@ import jsonpickle
 
 from channels.generic.websocket import WebsocketConsumer
 
-from .world import World
+import functools
+
+from .world import World, Entity
 
 world = World(20, 20)
 world.generate()
 
+players = []
+
+class Player:
+    def __init__(self, consumer):
+        self.consumer = consumer
+        self.entity = Entity()
+        world.add_entity(self.entity)
+
+    def update(self):
+        self.consumer.update()
+
+    def on_remove(self):
+        self.entity.remove()
+
 class RoguelikeConsumer(WebsocketConsumer):
     def connect(self):
         self.handlers = {
-            "auth": self.auth
+            "auth": self.on_auth
         }
 
         self.accept();
 
-        self.respond("update", world.get_sprites())
-
     def disconnect(self, close_code):
-        pass
+        if self.player:
+            self.player.on_remove()
+            players.remove(self.player)
 
     def receive(self, text_data):
         decoded = jsonpickle.decode(text_data)
@@ -40,9 +56,17 @@ class RoguelikeConsumer(WebsocketConsumer):
             "text": text
         })
 
-    def auth(self, data):
-        if data:
-            self.send_message("Server", "Successfully authorized")
-        else:
-            self.send_message("Server", "hmm?")
+    def update(self):
+        self.respond("update", world.get_sprites())
+
+    def on_auth(self, data):
+        if not data:
             self.close()
+            return
+
+        self.send_message("Server", "Successfully authorized")
+        self.player = Player(self)
+        players.append(self.player)
+
+        for player in players:
+            player.update()
--- mp_roguelike/world.py
+++ mp_roguelike/world.py
@@ -23,22 +23,66 @@ class Wall(Tile):
 
         self.impassable = True
 
+class Entity(Tile):
+    colors = ["red", "green", "blue", "yellow", "orange", "magenta", "cyan"]
+
+    def __init__(self):
+        super().__init__(Sprite("@", random.choice(self.colors)))
+
+        self.x = -1
+        self.y = -1
+
+    def remove(self):
+        self.world.remove_entity(self)
+
+    def random_position(self):
+        while self.world.is_occupied(self.x, self.y):
+            self.x = random.randint(0, self.world.width)
+            self.y = random.randint(0, self.world.height)
+
+    def is_at(self, x, y):
+        return self.x == x and self.y == y
+
+    def on_add(self, world):
+        self.world = world
+        self.random_position()
+
+    def on_remove(self):
+        self.world = None
+
 class World:
     def __init__(self, width, height):
         self.width = width
         self.height = height
         self.tiles = []
+        self.entities = []
 
     def get_tile_at(self, x, y):
-        if self.tiles and self.is_in_bounds(x, y):
+        if self.is_in_bounds(x, y):
             return self.tiles[y][x]
         return Tile()
+
+    def get_entities_at(self, x, y):
+        if self.is_in_bounds(x, y):
+            return [entity for entity in self.entities if entity.is_at(x, y)]
+        return []
 
     def is_on_border(self, x, y):
         return x == 0 or y == 0 or x == self.width - 1 or y == self.height - 1
 
     def is_in_bounds(self, x, y):
-        return x >= 0 and y >= 0 and x < width and y < height
+        return x >= 0 and y >= 0 and x < self.width and y < self.height
+
+    def is_occupied(self, x, y):
+        return not self.is_in_bounds(x, y) or self.get_tile_at(x, y).impassable
+
+    def add_entity(self, entity):
+        entity.on_add(self)
+        self.entities.append(entity)
+
+    def remove_entity(self, entity):
+        entity.on_remove()
+        self.entities.remove(entity)
 
     def get_sprites(self):
         sprites = []
@@ -46,8 +90,11 @@ class World:
         for y, row in enumerate(self.tiles):
             sprites.append([])
 
-            for tile in row:
+            for x, tile in enumerate(row):
                 sprites[y].append(tile.sprite)
+
+                for entity in self.get_entities_at(x, y):
+                    sprites[y][x] = entity.sprite
 
         return sprites
 
