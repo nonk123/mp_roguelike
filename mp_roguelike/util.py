238f580cf2964002cac13a9d2c0dc3dc5ded672d
nonk123 <Sergeyii321@gmail.com>
2020-04-17 17:18:10 +0300

    Slightly improve Die class

--- mp_roguelike/util.py
+++ mp_roguelike/util.py
@@ -4,10 +4,12 @@ def color(color, text):
     return f'<span style="color: {color};">{text}</span>'
 
 class Die:
-    def __init__(self, count, sides, inc):
+    def __init__(self, count, sides, inc=0):
         self.count = count
         self.sides = sides
         self.inc = inc
+
+        self.roll = self.__call__
 
     def __call__(self, ontop=0):
         roll = 0
@@ -16,6 +18,3 @@ class Die:
             roll += random.randint(1, self.sides)
 
         return roll + self.inc + ontop
-
-    def roll(self, ontop=0):
-        return self(ontop)
